c33445bff8539c93e3907815da306dd910eff000
gyrotron <simmarkalsi@gmail.com>
2019-08-25 01:59:47 +0530

    bill details

--- pages/bills/index.js
+++ pages/bills/index.js
@@ -1,58 +1,107 @@
 import React, { Component } from "react";
 import Layout from "./../../components/Layouts/Layouts";
-export default class Bill extends Component {
-  state = {
-  };
-  constructor(props) {
-    super(props);
+import Bill from "../../etherium/bill";
+import { Card, Button, Grid, GridColumn } from "semantic-ui-react";
+import { Link } from "../../routes";
+export default class BillShow extends Component {
+  static async getInitialProps(props) {
+    const bill = Bill(props.query.address);
+    const summary = await bill.methods.getSummary().call();
+    const billDescription = await bill.methods.billDescription().call();
+    const billDetails = await bill.methods.billDetails().call();
+    const ipfsHash = await bill.methods.ipfsHash().call();
+    return {
+      address: props.query.address,
+      plansCount: summary[0],
+      votersCount: summary[1],
+      politician: summary[2],
+      billDescription,
+      billDetails,
+      ipfsHash
+    };
   }
-  render () {
+
+  render() {
     return (
       <Layout title='Bill'>
-        <section style={{ paddingTop: 100 }} className="section-rt">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8">
-                <div className="rt-title">
+        <section style={{ paddingTop: 100 }} className='section-rt'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-8'>
+                <div className='rt-title'>
                   <div className='rt-title-logo'>
-                    <img className="img-fluid" src="/static/img/bill.png" alt="Bill"></img>
+                    <img
+                      className='img-fluid'
+                      src='/static/img/bill.png'
+                      alt='Bill'
+                    ></img>
                   </div>
-                  <div className='rt-title-text'>Bill information</div>
+                  <div className='rt-title-text'>
+                    {this.props.billDescription}
+                  </div>
                 </div>
               </div>
-              <div className="col-md-4">
-                <div className="card card-rt">
-                  Note: Before you vote, carefully go through the description and the attached files. They will make sure you make the best decision.
+              <div className='col-md-4'>
+                <div className='card card-rt'>
+                  Note: Before you vote, carefully go through the description
+                  and the attached files. They will make sure you make the best
+                  decision.
                 </div>
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-12">
+            <div className='row'>
+              <div className='col-md-6'>
                 <div>Description:</div>
-                <div className="card card-rt">
-                  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla  dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla</p>
+                <div className='card card-rt'>
+                  <p>{this.props.billDetails}</p>
                 </div>
-              </div>
-            </div>
-            <div className="row mt-3 justify-content-between">
-            <div className="col-md-9">
-                <div className="card card-rt">
-                  <div className="d-flex">
-                  <div style={{height:'100px', width:'100px', backgroundColor: '#555', marginRight: '20px'}}></div>
-                  <div style={{height:'100px', width:'100px', backgroundColor: '#555', marginRight: '20px'}}></div>
-                  <div style={{height:'100px', width:'100px', backgroundColor: '#555', marginRight: '20px'}}></div>
-                  <div style={{height:'100px', width:'100px', backgroundColor: '#555'}}></div>
+                <div className='col-sm-12 card card-rt'>
+                  <div className='info'>
+                    <span className='key'>Name of the Politician</span>
+                    <span className='value'>Politician 1</span>
+                  </div>
+                  <div className='info'>
+                    <span className='key'>Address of the Politician</span>
+                    <span className='value'>{this.props.politician}</span>
+                  </div>
+                  <div className='col-sm-12'>
+                    <div className='info'>
+                      <span className='key'>Plans in a bill</span>
+                      <span className='value'>{this.props.plansCount}</span>
+                    </div>
+                  </div>
+                  <div className='col-sm-12'>
+                    <div className='info'>
+                      <span className='key'>Number of voters</span>
+                      <span className='value'>{this.props.votersCount}</span>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="col-md-2">
-                <div className="card card-rt">
-                  <button className="btn btn-rt success">View plans</button>
+              {/* <div className="row mt-3 justify-content-between"> */}
+              <div className='col-md-6'>
+                <div className='card card-rt'>
+                  <div className='d-flex'>
+                    <img
+                      src={`https://ipfs.io/ipfs/${this.props.ipfsHash}`}
+                      width='100'
+                      height='100'
+                    />
+                  </div>
+                </div>
+                <div className='col-md-2'>
+                  <div className='card card-rt'>
+                    <Link route={`/bills/${this.props.address}/plans`}>
+                      <a>
+                        <Button primary>View Plans</Button>
+                      </a>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="d-flex justify-content-center mt-5">
-              <div className="rt-bar primary"></div>
+            <div className='d-flex justify-content-center mt-5'>
+              <div className='rt-bar primary'></div>
             </div>
           </div>
         </section>
--- routes.js
+++ routes.js
@@ -1,10 +1,9 @@
 const routes = require("next-routes")();
 
-
 routes.add("/bills/new", "/bills/new"); // TODO: Change (Simmar)
-routes.add("/bills/:address", "/bills/show"); // TODO: Change (Simmar)
+routes.add("/bills/:address", "/bills/index"); // TODO: Change (Simmar)
 routes.add("/bills/:address/plans", "/bills/plans/index"); // TODO: Change (Simmar)
 routes.add("/bills/:address/plans/new", "/bills/plans/new"); // TODO: Change (Simmar)
-// routes.add("/bills/:address", "/bills/show"); 
+// routes.add("/bills/:address", "/bills/show");
 
 module.exports = routes;
