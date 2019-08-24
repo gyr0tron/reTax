501037be4246b5ed501f9efce5d0091fbfc18171
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-08-24 18:30:25 +0530

    Updated query

--- pages/query.js
+++ pages/query.js
@@ -15,25 +15,26 @@ class Query extends Component {
       <Layout title='Welcome to reTax'>
         <section style={{paddingTop:100}}>
           <div className="container">
-            <div className="row">
-              <div className="col-sm-8">
-                <div className="card">
-                  <div className="card">
-                    <div className="card-body">
-                      <h2 className="text-primary">Make your voice heard</h2>
-                      <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Write a topic for your query" />
-                      </div>
-                      <div className="form-group">
-                        <textarea className="form-control" rows="4" cols="50" placeholder="Give detail information about your query">
-                        </textarea>
-                      </div>
-                      <div className="form-group">
-                        <input type="file" multiple>
-                        </input>
-                      </div>
-                      <button className="btn btn-primary">Submit</button>
+            <div className="card">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-sm-8">
+                    <h2 className="text-primary">Make your voice heard</h2>
+                    <div className="form-group">
+                      <input type="text" className="form-control" placeholder="Write a topic for your query" />
                     </div>
+                    <div className="form-group">
+                      <textarea className="form-control" rows="4" cols="50" placeholder="Give detail information about your query">
+                      </textarea>
+                    </div>
+                    <div className="form-group">
+                      <input type="file" multiple>
+                      </input>
+                    </div>
+                    <button className="btn btn-primary">Submit</button>
+                  </div>
+                  <div className="col-sm-4">
+                    <img id='logo' src='/static/img/voice.svg' style={{margin:15}}/>
                   </div>
                 </div>
               </div>
