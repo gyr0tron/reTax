9e980061ebab092c1867bc28827d23e5cf372978
Bhupender Singh <bhpndrsingh306@gmail.com>
2019-08-25 02:14:33 +0530

    Add new plan page

--- pages/bills/plans/new.js
+++ pages/bills/plans/new.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Layout from "./../../components/Layouts/Layouts";
+import Layout from "./../../../components/Layouts/Layouts";
 export default class BillPlanNew extends Component {
   state = {
   };
@@ -11,16 +11,36 @@ export default class BillPlanNew extends Component {
       <Layout title='Bills | Plans'>
         <section style={{ paddingTop: 100 }} className="section-rt">
           <div className="container">
+            <div className="text-left" style={{cursor: 'pointer'}}>Back</div>
             <div className="row">
               <div className="col-md-12">
                 <div className="rt-title">
                   <div className='rt-title-logo'>
                     <img className="img-fluid" src="/static/img/bill.png" alt="Bill"></img>
                   </div>
-                  <div className='rt-title-text'>Tax payment</div>
+                  <div className='rt-title-text'>New Plan</div>
                 </div>
               </div>
             </div>
+            <div className="row">
+              <div className="col-md-12">
+                Desription <br/>
+                <input className="form-control" type="text" placeholder="Enter description" />
+              </div>
+              <div className="col-md-12">
+                Value (in Ether) <br/>
+                <input className="form-control" type="text" placeholder="Enter value" />
+              </div>
+              <div className="col-md-12">
+                Contracting company's wallet <br/>
+                <input className="form-control" type="text" placeholder="Enter address" />
+              </div>
+              <div className="col-md-12">
+                Contracting company's wallet <br/>
+                <button className="btn btn-primary">Create new plan</button>
+              </div>
+            </div>
+            
           </div>
         </section>
       </Layout>
