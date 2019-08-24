e92c63b1fbbbab6fa812fe007816f389856ad47b
Bhupender Singh <bhpndrsingh306@gmail.com>
2019-08-25 03:35:06 +0530

    Paytax page done

--- pages/paytax.js
+++ pages/paytax.js
@@ -6,7 +6,7 @@ import web3 from "../etherium/web3";
 import { Router } from "../routes";
 
 export default class BillPlanNew extends Component {
-  static async getInitialProps() {
+  static async getInitialProps () {
     const minimumTax = await generator.methods.minimumTax().call();
     return { minimumTax };
   }
@@ -33,67 +33,55 @@ export default class BillPlanNew extends Component {
     this.setState({ loading: false });
   };
 
-  render() {
+  render () {
     return (
       <Layout title='Bills | Plans'>
         <section style={{ paddingTop: 100 }} className='section-rt'>
           <div className='container'>
-            <div className='row'>
-              <div className='col-md-12'>
-                <div className='rt-title'>
-                  <div className='rt-title-logo'>
+            <div className="card card-rt">
+              <div className="card-body">
+                <div className='row'>
+                  <div className='col-md-8'>
+                    <div className='rt-title mb-3'>
+                      <div className="rt-title-text">Tax payment</div>
+                    </div>
+                    <div className='rt-subtitle'>For registering yourself as a <strong>voter</strong> you need to pay the minimum tax assigned to you.</div>
+
+                    <div className="my-3"><strong>Minimum Tax:{" "}{web3.utils.fromWei(this.props.minimumTax, "Ether")}</strong>{" "}Ether</div>
+                    <div className="container-fluid">
+                      <div className="row">
+                        <div className="col-md-10 px-0">
+                          <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+                            <Form.Field>
+                              <Input
+                                label='Ether'
+                                labelPosition='right'
+                                value={this.state.tax}
+                                onChange={event =>
+                                  this.setState({ tax: event.target.value })
+                                }
+                              />
+                            </Form.Field>
+                            <Message
+                              error
+                              header='Error'
+                              content={this.state.errorMessage}
+                            />
+                            <Button className="btn-rt semantic primary" loading={this.state.loading} primary> Pay </Button>
+                          </Form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4">
                     <img
                       className='img-fluid'
                       src='/static/img/money.svg'
                       alt='Bill'
-                    ></img>
-                  </div>
-                  <div className='rt-title-text'>Tax payment</div>
-                </div>
-                <div className='rt-subtitle'>
-                  For registering yourself as a <strong>Voter</strong> you need
-                  to pay the minimum tax assigned to you.
-                </div>
-              </div>
-            </div>
-            <div className='row'>
-              {this.state.taxInput}
-              <div className='col-md-12'>
-                <div>
-                  <strong>
-                    Minimum Tax:{" "}
-                    {web3.utils.fromWei(this.props.minimumTax, "Ether")}
-                  </strong>{" "}
-                  Ether
-                </div>
-                {/* <input className="form-control" type="text" />
-                <div>
-                  <button className="btn btn-rt primary">Pay</button>
-                </div> */}
-                <Form
-                  onSubmit={this.onSubmit}
-                  error={!!this.state.errorMessage}
-                >
-                  <Form.Field>
-                    <Input
-                      label='Ether'
-                      labelPosition='right'
-                      value={this.state.tax}
-                      onChange={event =>
-                        this.setState({ tax: event.target.value })
-                      }
+                      style={{ height: '300px' }}
                     />
-                  </Form.Field>
-
-                  <Message
-                    error
-                    header='Error'
-                    content={this.state.errorMessage}
-                  />
-                  <Button loading={this.state.loading} primary>
-                    Pay
-                  </Button>
-                </Form>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
