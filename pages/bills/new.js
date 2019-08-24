a0b8ad2e845e396de2cf391a50308f02e49af19f
Bhupender Singh <bhpndrsingh306@gmail.com>
2019-08-25 00:57:59 +0530

    Bill New

--- pages/bills/new.js
+++ pages/bills/new.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import $ from "jquery";
 
 import Layout from "./../../components/Layouts/Layouts";
 
-class BillNew extends Component {
+class Query extends Component {
   state = {
 
   };
@@ -13,7 +14,32 @@ class BillNew extends Component {
     return (
       <Layout title='Welcome to reTax'>
         <section style={{paddingTop:100}}>
-          Bill New
+          <div className="container">
+            <div className="card">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-sm-8">
+                    <h2 className="text-primary">Create a new Bill</h2>
+                    <div className="form-group">
+                      <input type="text" className="form-control" placeholder="Topic" />
+                    </div>
+                    <div className="form-group">
+                      <textarea className="form-control" rows="4" cols="50" placeholder="Description">
+                      </textarea>
+                    </div>
+                    <div className="form-group">
+                      <input type="file" multiple>
+                      </input>
+                    </div>
+                    <button className="btn btn-primary">Create</button>
+                  </div>
+                  <div className="col-sm-4">
+                    <img id='logo' src='/static/img/bill.png' style={{margin:15, width: '60%'}}/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
       </Layout>
     );
