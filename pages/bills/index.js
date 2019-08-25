0eb58f2a69a40b08e3e031254fe624acb9130ddf
Bhupender Singh <bhpndrsingh306@gmail.com>
2019-08-25 05:39:16 +0530

    View bills page

--- pages/bills/index.js
+++ pages/bills/index.js
@@ -4,7 +4,7 @@ import Bill from "../../etherium/bill";
 import { Card, Button, Grid, GridColumn } from "semantic-ui-react";
 import { Link } from "../../routes";
 export default class BillShow extends Component {
-  static async getInitialProps(props) {
+  static async getInitialProps (props) {
     const bill = Bill(props.query.address);
     const summary = await bill.methods.getSummary().call();
     const billDescription = await bill.methods.billDescription().call();
@@ -21,87 +21,75 @@ export default class BillShow extends Component {
     };
   }
 
-  render() {
+  render () {
     return (
       <Layout title='Bill'>
         <section style={{ paddingTop: 100 }} className='section-rt'>
           <div className='container'>
-            <div className='row'>
-              <div className='col-md-8'>
-                <div className='rt-title'>
-                  <div className='rt-title-logo'>
-                    <img
-                      className='img-fluid'
-                      src='/static/img/bill.png'
-                      alt='Bill'
-                    ></img>
-                  </div>
-                  <div className='rt-title-text'>
-                    {this.props.billDescription}
-                  </div>
-                </div>
-              </div>
-              <div className='col-md-4'>
-                <div className='card card-rt'>
-                  Note: Before you vote, carefully go through the description
-                  and the attached files. They will make sure you make the best
-                  decision.
-                </div>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col-md-6'>
-                <div>Description:</div>
-                <div className='card card-rt'>
-                  <p>{this.props.billDetails}</p>
-                </div>
-                <div className='col-sm-12 card card-rt'>
-                  <div className='info'>
-                    <span className='key'>Name of the Politician</span>
-                    <span className='value'>Politician 1</span>
-                  </div>
-                  <div className='info'>
-                    <span className='key'>Address of the Politician</span>
-                    <span className='value'>{this.props.politician}</span>
-                  </div>
-                  <div className='col-sm-12'>
-                    <div className='info'>
-                      <span className='key'>Plans in the bill</span>
-                      <span className='value'>{this.props.plansCount}</span>
+            <div className="card card-rt px-2">
+              <div className="card-body">
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-7">
+                      <div className='rt-title'>
+                        <div className='rt-title-text'>
+                          {this.props.billDescription}
+                        </div>
+                      </div>
+                      <div className=" my-3">
+                        <div className="font-weight-bold">Description:</div>
+                        <div className="">{this.props.billDetails}</div>
+                      </div>
+                    </div>
+                    <div className="col-md-5">
+                      <div className='card card-rt mx-0 rt-shadow'>
+                        <span className="font-weight-bold">Note: </span>
+                        <span>Before you vote, carefully go through the description and the attached files. They will make sure you make the best decision.</span>
+                      </div>
                     </div>
                   </div>
-                  <div className='col-sm-12'>
-                    <div className='info'>
-                      <span className='key'>Number of voters</span>
-                      <span className='value'>{this.props.votersCount}</span>
+                  <div className="row">
+                    <div className="col-md-7">
+                      <div className="font-weight-bold mt-3">Details:</div>
+                      <div className="font-weight-light mb-2" style={{ fontSize: '.9rem', fontStyle: 'italic' }}>Relavant details of the bill which specify plans, politician incharge and number of total votes.</div>
+                      <div className="card card-rt rt-shadow mt-4">
+                        <div className='info'>
+                          <span className='key'>Name of the Politician</span>
+                          <span className='value'>Politician 1</span>
+                        </div>
+                        <div className='info'>
+                          <span className='key'>Address of the Politician</span>
+                          <span className='value'>{this.props.politician}</span>
+                        </div>
+                        <div className='info'>
+                          <span className='key'>Plans in the bill</span>
+                          <span className='value'>{this.props.plansCount}</span>
+                        </div>
+                        <div className='info'>
+                          <span className='key'>Number of voters</span>
+                          <span className='value'>{this.props.votersCount}</span>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-              {/* <div className="row mt-3 justify-content-between"> */}
-              <div className='col-md-6'>
-                <div className='card card-rt'>
-                  <div className='d-flex'>
-                    <img
-                      src={`https://ipfs.io/ipfs/${this.props.ipfsHash}`}
-                      width='100'
-                      height='100'
-                    />
-                  </div>
-                </div>
-                <div className='col-md-2'>
-                  <div className='card card-rt'>
-                    <Link route={`/bills/${this.props.address}/plans`}>
-                      <a>
-                        <Button primary>View Plans</Button>
+                    <div className="col-md-5">
+                      <div className="font-weight-bold mt-3">Documents:</div>
+                      <div className="font-weight-light" style={{ fontSize: '.9rem', fontStyle: 'italic' }}>Files uploaded by the creator of the bill to provide the context and need for it to pass.</div>
+                      <a target="_blank" href={`https://ipfs.io/ipfs/${this.props.ipfsHash}`} className="card card-rt rt-shadow mt-4">
+                        <img src={`https://ipfs.io/ipfs/${this.props.ipfsHash}`} width='auto' height='225px' />
                       </a>
-                    </Link>
+                    </div>
+                  </div>
+                  <div className="row my-3">
+                    <div className="col-md-12">
+                      <Link route={`/bills/${this.props.address}/plans`}>
+                        <a>
+                          <Button className="btn-rt primary semantic py-3">Explore Plans</Button>
+                        </a>
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className='d-flex justify-content-center mt-5'>
-              <div className='rt-bar primary'></div>
             </div>
           </div>
         </section>
